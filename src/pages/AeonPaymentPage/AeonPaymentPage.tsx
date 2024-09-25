a0d76bb9dc9afda81fab69ab00b2195f2f72ccb9
athevinha <nguyenthevinh1002@gmail.com>
2024-09-25 11:38:53 +0700

    feat: add aeon page

---
+++ src/pages/AeonPaymentPage/AeonPaymentPage.tsx
@@ -0,0 +1,12 @@
+import {List} from '@telegram-apps/telegram-ui';
+import type {FC} from 'react';
+
+
+export const AeonPaymentPage: FC = () => {
+
+  return (
+    <List>
+      Hello Aeon
+    </List>
+  );
+};
--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,12 +1,17 @@
 import {type FC} from "react";
 
-
+import {AeonPaymentPage} from "../AeonPaymentPage/AeonPaymentPage";
+import {InitDataPage} from "../InitDataPage/InitDataPage";
+import {LaunchParamsPage} from "../LaunchParamsPage/LaunchParamsPage";
+import {ThemeParamsPage} from "../ThemeParamsPage/ThemeParamsPage";
 import "./IndexPage.css";
 export const IndexPage: FC = () => {
-
   return (
     <div>
-      Hello worlds
+      <AeonPaymentPage/>
+      <InitDataPage />
+      <LaunchParamsPage />
+      <ThemeParamsPage />
     </div>
   );
 };
