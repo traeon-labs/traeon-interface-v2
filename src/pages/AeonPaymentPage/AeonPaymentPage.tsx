578aca0b5355b21a51a4d1d3df6a544180ecf812
athevinha <nguyenthevinh1002@gmail.com>
2024-09-26 13:27:32 +0700

    order sign

--- src/pages/AeonPaymentPage/AeonPaymentPage.tsx
+++ src/pages/AeonPaymentPage/AeonPaymentPage.tsx
@@ -1,30 +1,53 @@
-import useSignSHA512 from "@/hook/useSign";
+import useSignSHA512 from "@/pages/AeonPaymentPage/hook/useSignSHA512";
+import {createAeonOrdersWithTma} from "@/utils/aeon/createOrder";
 import {Button,Cell,List,Section} from "@telegram-apps/telegram-ui";
 import {type FC} from "react";
-const params = JSON.parse(JSON.stringify({
-  appId: "TEST000001",
-  // sign: "TEST000001", // Excluded from signature generation
-  merchantOrderNo: "11126",
-  userId: "505884978@qq.com",
-  orderAmount: "1000",
-  payCurrency: "USD",
-  paymentTokens: "USDT,ETH",
-  paymentExchange:
-    "16f021b0-f220-4bbb-aa3b-82d423301957,9226e5c2-ebc3-4fdd-94f6-ed52cdce1420",
-}));
+const params = JSON.parse(
+  JSON.stringify({
+    appId: "TEST000001",
+    // sign: "TEST000001", // Excluded from signature generation
+    merchantOrderNo: "11126",
+    userId: "505884978@qq.com",
+    orderAmount: "1000",
+    payCurrency: "USD",
+    paymentTokens: "USDT,ETH",
+    paymentExchange:
+      "16f021b0-f220-4bbb-aa3b-82d423301957,9226e5c2-ebc3-4fdd-94f6-ed52cdce1420",
+  })
+);
 export const AeonPaymentPage: FC = () => {
-  const sign = useSignSHA512({data: params})
+  const sign = useSignSHA512({ data: params });
+  const createOrdersTelegram = async () => {
+    const params = {
+      appId: "6fdbaac29eb94bc6b12345ad705e9293",
+      callbackURL: "https://crypto-payment-sbx.aeon.cc/crypto/bot/cpCallback",
+      customParam:
+        '{"botName":"ABCDCryptoPaymentTestBot","orderDetail":"ABDC Test BAG 🎒","chatId":"6831529261"}',
+      expiredTime: "999999",
+      merchantOrderNo: "17243134568514",
+      orderAmount: "10",
+      orderModel: "ORDER",
+      payCurrency: "USD",
+      sign: "TEST000001",
+      tgModel: "MINIAPP",
+      userId: "5123456978@qq.com",
+    };
+    try {
+      const res = await createAeonOrdersWithTma(params)
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+  
+  };
   return (
     <List>
       <Section header="Aeon Payments">
         <Cell>Clothes</Cell>
-        <Button className="w-100">Pay with Aeon $1</Button>
+        <Button className="w-100" onClick={createOrdersTelegram}>
+          Pay with Aeon $1
+        </Button>
         {sign}
-        {/* <Cell subtitle={error ? error : sign}>
-          <Typography>
-            Hash
-          </Typography>
-        </Cell> */}
       </Section>
     </List>
   );
--- src/utils/aeon/createOrder.ts
+++ src/utils/aeon/createOrder.ts
@@ -1,5 +1,44 @@
-export const createOrderBot = () => {
+import axios from 'axios';
+import * as CryptoJS from 'crypto-js';
+import {generateSignature} from '../sign';
+import {AEON_SANDBOX_PAYMENTS_BASE_API, AEON_SIGN_KEY} from '@/config';
+
+// Define the request parameters interface
+interface RequestParams {
+  appId: string;
+  merchantOrderNo: string;
+  orderAmount: string;
+  payCurrency: string;
+  userId: string;
+  paymentExchange?: string;
+  paymentTokens?: string;
+  redirectURL?: string;
+  callbackURL?: string;
+  customParam?: string;
+  expiredTime?: string;
+  payType?: string;
+  paymentNetworks?: string;
+  orderModel?: string;
+  tgModel?: string;
+  sign?: string;
+}
+
+// Define the API request function
+export async function createAeonOrdersWithTma(params: RequestParams) {
+  const requestParams: RequestParams = params
+
+  // Generate the signature
+  requestParams.sign = generateSignature(JSON.parse(JSON.stringify(params)), AEON_SIGN_KEY);
+  console.log('requestParams', requestParams)
   try {
-    const path = "/open/api/tgPayment";
-  } catch (error) {}
-};
+    const response = await axios.post(`${AEON_SANDBOX_PAYMENTS_BASE_API}/open/api/tgPayment`, requestParams, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    console.log('Response data:', response.data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
