5219ba9547ef45a641dc3aee7419bb465f358c1c
athevinha <nguyenthevinh1002@gmail.com>
2024-09-27 12:35:43 +0700

    tabs state

--- src/pages/MarketplacePage/MarketplacePage.tsx
+++ src/pages/MarketplacePage/MarketplacePage.tsx
@@ -2,12 +2,23 @@ import marketplaceAssets from "@/_mock/metadata/nfts.json";
 import { IMarketplaceFilterTabs, INFTMetadata } from "@/types/index.type";
 import { Grid2 } from "@mui/material";
 import { List } from "@telegram-apps/telegram-ui";
-import { FC, useState } from "react";
+import { FC, useMemo, useState } from "react";
 import { AssetCard } from "./AssetCard";
 import { MarketplaceFilter } from "./MarketplaceFilter";
+import {MARKETPLACE_ASSET_CONFIG} from "@/utils/constant";
 export const MarketplacePage: FC = () => {
   const [marketplaceTab, setMarketplaceTab] =
     useState<IMarketplaceFilterTabs>("ph:t-shirt-bold");
+    
+  const marketplaceAssetsFilter = useMemo(() => {
+    return marketplaceAssets.filter((asset) => {
+      const typeAttributes = asset.attributes.filter(
+        (att) => att.trait_type === "type"
+      )[0];
+      if (MARKETPLACE_ASSET_CONFIG?.[typeAttributes?.value]?.icon === marketplaceTab) return true;
+      return false;
+    });
+  }, [marketplaceTab]);
   return (
     <List style={{ textAlign: "center" }}>
       <MarketplaceFilter
@@ -15,7 +26,7 @@ export const MarketplacePage: FC = () => {
         setMarketplaceTab={setMarketplaceTab}
       />
       <Grid2 container spacing={3}>
-        {marketplaceAssets.map((assetItem: INFTMetadata, _) => {
+        {marketplaceAssetsFilter.map((assetItem: INFTMetadata, _) => {
           return <AssetCard item={assetItem} key={_} />;
         })}
       </Grid2>
