58398bc483e59e6d9ea40683632a355f0b8b428e
athevinha <nguyenthevinh1002@gmail.com>
2024-10-11 19:47:07 +0700

    config screen

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,22 +1,24 @@
 import { useEffect, useState, type FC } from "react";
 
+import { INFTMetadata, ITabs } from "@/types/index.type";
+import { AeonPaymentModal } from "../AeonPaymentPage/components/AeonPaymentModal";
+import AssetModal from "../AssetModal/AssetModal";
 import { MarketplacePage } from "../MarketplacePage/MarketplacePage";
 import { TabsController } from "../MarketplacePage/TabsController";
-import "./IndexPage.css";
+import { TravelPage } from "../TravelPage/TravelPage";
 import { AccountPopover } from "./AccountPropover";
-import { INFTMetadata, ITabs } from "@/types/index.type";
-import AssetModal from "../AssetModal/AssetModal";
-import {TravelPage} from "../TravelPage/TravelPage";
-import {AeonPaymentPage} from "../AeonPaymentPage/AeonPaymentPage";
-import {AeonPaymentModal} from "../AeonPaymentPage/components/AeonPaymentModal";
+import "./IndexPage.css";
+import { init, backButton, postEvent } from '@telegram-apps/sdk';
+init()
+postEvent('web_app_setup_swipe_behavior', {allow_vertical_swipe: false})
 export const IndexPage: FC = () => {
-  const [tab, setTab] = useState<ITabs>('mdi:location-on-outline');
+  const [tab, setTab] = useState<ITabs>("mdi:location-on-outline");
   const [assetModal, setAssestModal] = useState<boolean>(false);
   const [travelMapModal, setTravelMapModal] = useState<boolean>(false);
-  const [currentAsset, setCurrentAsset] = useState<INFTMetadata | undefined>()
+  const [currentAsset, setCurrentAsset] = useState<INFTMetadata | undefined>();
   useEffect(() => {
-    console.log(assetModal)
-  },[assetModal])
+    console.log(assetModal);
+  }, [assetModal]);
   return (
     <div>
       {/* <AeonPaymentPage/> */}
@@ -24,19 +26,29 @@ export const IndexPage: FC = () => {
 
       <AccountPopover />
       {tab === "mdi:shopping-outline" ? (
-        <MarketplacePage  setCurrentAsset={setCurrentAsset} visible={assetModal} setVisible={setAssestModal} asset={currentAsset}/>
+        <MarketplacePage
+          setCurrentAsset={setCurrentAsset}
+          visible={assetModal}
+          setVisible={setAssestModal}
+          asset={currentAsset}
+        />
       ) : tab === "akar-icons:thunder" ? (
         "akar"
       ) : tab === "iconamoon:certificate-badge" ? (
         "badge"
       ) : tab === "mdi:location-on-outline" ? (
-        <TravelPage visible={travelMapModal} setVisible={setTravelMapModal}/>
+        <TravelPage visible={travelMapModal} setVisible={setTravelMapModal} />
       ) : (
         ""
       )}
-      <AeonPaymentModal/>
+      <AeonPaymentModal />
       <TabsController tab={tab} setTab={setTab} />
-      <AssetModal visible={assetModal} setVisible={setAssestModal} asset={currentAsset} setCurrentAsset={setCurrentAsset}/> 
+      <AssetModal
+        visible={assetModal}
+        setVisible={setAssestModal}
+        asset={currentAsset}
+        setCurrentAsset={setCurrentAsset}
+      />
     </div>
   );
 };
